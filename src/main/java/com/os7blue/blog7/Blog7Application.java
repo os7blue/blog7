47cb7a40a514b0e0d4f9671ad31da208a7e18c4c
os7blue <os7blue@163.com>
2019-05-08 10:41:07 +0800

    1

---
+++ src/main/java/com/os7blue/blog7/Blog7Application.java
@@ -0,0 +1,13 @@
+package com.os7blue.blog7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blog7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/os7blue/blog7/Blog7ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.os7blue.blog7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Blog7ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
